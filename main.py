b17048c60adac67b712048a66f5ad674a49ea102
lunar0x4 <skilled.chair52@gmail.com>
2026-03-20 15:29:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
     print_banner()
 
     print(f"{Fore.GREEN}[1] {Fore.WHITE}hyper3d.ai")
+    print(f"{Fore.CYAN}[1] {Fore.WHITE}piclumen.py")
     print(f"{Fore.RED}[0] {Fore.WHITE}exit\n")
 
     try:
@@ -26,6 +27,8 @@ def main():
 
         if choice == "1":
             subprocess.run([sys.executable, "gens/hyper3d.py"])
+        elif choice == "2":
+            subprocess.run([sys.executable, "gens/piclumen.py"])
         elif choice == "0":
             print(f"\n{Fore.GREEN}goodbye!")
             sys.exit(0)
